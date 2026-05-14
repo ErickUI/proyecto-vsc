5ba766667e8052d16a709463478053b0ca5a68d2
ErickUI <samirerickquintanacanorio@gmail.com>
2026-05-14 12:55:57 -0500

    fix: corregir error en suma

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,10 @@
-function suma(a, b) {
- return a + b;
-}
+
+const sumaTotal = (...numeros) => {
+  return numeros
+    .map(n => parseFloat(n)) // Intenta convertir todo a número (arregla strings)
+    .filter(n => !isNaN(n))  // Elimina lo que no se pudo convertir (arregla basura)
+    .reduce((acumulado, actual) => acumulado + actual, 0);
+};
+
+console.log(sumaTotal(1, 2, 3, 4));          // 10
+console.log(sumaTotal(5, "10", null, "abc")); // 15 
